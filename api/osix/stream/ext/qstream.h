42445c8a1831923ece1d7fcccf42b17244b4915c
Rehno Lindeque <errantkid@gmail.com>
2009-11-28 12:51:47 +0200

    Added extension files for qstream some time ago

---
+++ api/osix/stream/ext/qstream.h
@@ -0,0 +1,23 @@
+#ifndef __OSIXEXT_QSTREAM_H__
+#define __OSIXEXT_QSTREAM_H__
+//////////////////////////////////////////////////////////////////////////////
+//
+//    QSTREAM.H
+//
+//    Author:      Rehno Lindeque
+//
+//    Description: QStream extensions
+//
+//    Version: 0.1 (2009-04-08)
+//
+//    Copyright © 2009, Rehno Lindeque. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////////
+/*                               DOCUMENTATION                              */
+/*
+    DESCRIPTION:
+      C interface for QStream extensions.
+*/
+/*                                   TYPES                                  */
+
+#endif
---
+++ api/osix/stream/ext/qstream.hpp
@@ -0,0 +1,27 @@
+#ifndef __OSIXEXT_QSTREAM_HPP__
+#define __OSIXEXT_QSTREAM_HPP__
+//////////////////////////////////////////////////////////////////////////////
+//
+//    QSTREAM.HPP
+//
+//    Author:      Rehno Lindeque
+//
+//    Description: QStream extensions
+//
+//    Version: 0.1 (2009-04-08)
+//
+//    Copyright © 2009, Rehno Lindeque. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////////
+/*                               DOCUMENTATION                              */
+/*
+    DESCRIPTION:
+      C++ interface for QStream extensions.
+*/
+/*                                 INCLUDES                                 */
+namespace QStream 
+{ 
+#include "qstream.h"
+}
+
+#endif
